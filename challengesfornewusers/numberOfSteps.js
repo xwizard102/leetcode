babb2823f92387cbe67cd84ed43bac6fa6eb4a3e
Mina Nagy <mnagy@icinformatics.com>
2022-10-15 11:48:40 +0200

    update number of steps solution to use bitwise

--- challengesfornewusers/numberOfSteps.js
+++ challengesfornewusers/numberOfSteps.js
@@ -4,12 +4,17 @@
  */
  var numberOfSteps = function(num) {
     
-    if (num === 0) return 0;
-    
-    return 1 + numberOfSteps(num%2? num-1 : num/2);
-    
+    var res = 0;
+
+    while(num) {
+        res += (num&1? 2 : 1);
+        num >>= 1;
+    }
+
+    return res? res-1 : res;
 };
 
 console.log(numberOfSteps(14));
 console.log(numberOfSteps(8));
 console.log(numberOfSteps(123));
+console.log(numberOfSteps(0));
