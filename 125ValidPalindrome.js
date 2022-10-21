c67b0ad93e610c3e91ea0ffb7f7ba9e2dcb289e8
Mina Nagy <mnagy@icinformatics.com>
2022-10-21 13:00:23 +0200

    add Valid Palindrome solution

---
+++ 125ValidPalindrome.js
@@ -0,0 +1,38 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+
+    s = sanitizeText(s).toLowerCase();
+
+    var mid = Math.floor(s.length/2);
+
+    for (let i=0; i<mid; i++) {
+        if (s[i] !== s[s.length-1-i]) return false;
+    }
+
+    return true;
+};
+
+function sanitizeText(s) {
+    for (let i=0; i<s.length; i++) {
+        if (!isAlphanumeric(s[i])) s = remove(s, i--);
+    }
+
+    return s;
+}
+
+function remove(s, i) {
+    return s.substring(0, i) + s.substring(i+1);
+}
+
+function isAlphanumeric(c) {
+    return (c >= '0' && c <= '9')
+        || (c >= 'a' && c <= 'z')
+        || (c >= 'A' && c <= 'Z');
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log(isPalindrome("race a car"));
+console.log(isPalindrome(" "));
